9ca3fc2b9645e3011667926554fbe5220afbe450
carolinecocca <ccocca1@umbc.edu>
2018-05-31 11:10:41 -0400

    Updated test code

--- NetworkAnalysis/Version0NA.py
+++ NetworkAnalysis/Version0NA.py
@@ -4,3 +4,17 @@
 ## Module: Network Analysis
 ## Title: Version0NA.py
 ############################
+
+class Node:
+    def __init__(self, label, size):
+        self.label = label
+        self.size = size
+
+    def print_summary(self):
+        print('Node', self.label, 'has the size', self.size)
+
+def main():
+    tester = Node('Umar', 4)
+    tester.print_summary()
+
+main()
